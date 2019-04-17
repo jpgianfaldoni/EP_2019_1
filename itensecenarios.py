a594e31a57cd43d4395433b4e9535b8c8e96af1e
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-04-17 14:14:05 -0300

    Corrige identacao e corrige dados

--- itensecenarios.py
+++ itensecenarios.py
@@ -58,6 +58,7 @@ def carregar_cenarios(nome):
             return carrega
 
 def main():
+    dados = 0
     print("Na hora do sufoco!")
     print("------------------")
     print()
@@ -77,23 +78,23 @@ def main():
         tamanho=len('Saguao')
         print('-'*tamanho)
         print('Você está no saguão do insper')
-        ecolha=input('Quer entrar nessa aventura?')
+        escolha=input('Quer entrar nessa aventura?')
         if escolha=='sim':                                  #########lembrar de colocar else
-                inicio=carregar_cenarios('saguao')
-                if inicio=='cenario1':
-                    print('Nessa sala temos esses itens:')
-                    print(itens_no_cenario('Saguão do Perigo'))
-                    print('O número sorteado no dado foi',dado)
-                    if dado()<=7:
-                        inventario.append('Mapa da DP')
-                        print('Você encontrou o Mapa da DP')        ###### condições de dado
-                        print('-5 de vida a cada rodada')
-                    elif dado()>=12:
-                        inventario.append('Chave da sala do Raul')
-                        print('Você encontrou a chave da sala do Raul')
-                        print('Use a com sabedoria')
-                        
-                opcoes=input()        
+            inicio=carregar_cenarios('saguao')
+            if inicio=='cenario1':
+                print('Nessa sala temos esses itens:')
+                print(itens_no_cenario('Saguão do Perigo'))
+                rng = dado()
+                print('O número sorteado no dado foi',dados)
+                if dados<=7:
+                    inventario.append('Mapa da DP')
+                    print('Você encontrou o Mapa da DP')        ###### condições de dado
+                    print('-5 de vida a cada rodada')
+                elif dados>=12:
+                    inventario.append('Chave da sala do Raul')
+                    print('Você encontrou a chave da sala do Raul')
+                    print('Use a com sabedoria')
+        opcoes=input()        
                         
                         
                         
@@ -105,34 +106,36 @@ def main():
                         
                         
                         
-            elif cenario_atual=='andar professor': #acontecimentos andar do professor
-                if 'Chave da sala do Raul' not in inventario:
-                    print('Você não consegue entrar na sala do Raul')
-                else:
-                    print('Nessa sala temos esses itens:')
-                    print(itens_no_cenario('Andar do Professor'))
-                    print('O número sorteado no dado foi',dado)
-                    if dado()>=15:
-                        hit_marker=caneta_do_Raul  #### hitmarker permannete 
-                        inventario.append('Caneta do Raul')
-                        inventario.append('Mapa para o Livro da Salvação')      ########## condições de dado
-                        print('Você foi abençoado com a caneta do Raul e encontrou o Mapa secreto para o Livro da Salvação')
-                        print('Hit Marker foi ZERADO')
-                    elif dado()<=10:
-                        hit_marker+=-pendrive_da_punicao  ###hitmarker permannete 
-                        inventario.append('Pendrive da Punição')
-                        print('Você irritou o MESTRE DO PYTHON')
-                        print('Por isso foi amaldiçoado com -2 pontos de vida a cada rodada')
+        if cenario_atual=='andar professor': #acontecimentos andar do professor
+            if 'Chave da sala do Raul' not in inventario:
+                print('Você não consegue entrar na sala do Raul')
+            else:
+                print('Nessa sala temos esses itens:')
+                print(itens_no_cenario('Andar do Professor'))
+                dados = dado()
+                print('O número sorteado no dado foi',dados)
+            if dados>=15:
+                hit_marker=caneta_do_Raul  #### hitmarker permannete 
+                inventario.append('Caneta do Raul')
+                inventario.append('Mapa para o Livro da Salvação')      ########## condições de dado
+                print('Você foi abençoado com a caneta do Raul e encontrou o Mapa secreto para o Livro da Salvação')
+                print('Hit Marker foi ZERADO')
+            elif dados<=10:
+                hit_marker+=-pendrive_da_punicao  ###hitmarker permannete 
+                inventario.append('Pendrive da Punição')
+                print('Você irritou o MESTRE DO PYTHON')
+                print('Por isso foi amaldiçoado com -2 pontos de vida a cada rodada')
             elif cenario_atual=='biblioteca': #acontecimentos biblioteca
                 print('Nessa sala temos esses itens:')
                 print(itens_no_cenario('Biblioteca'))
-                print('O número sorteado no dado foi',dado)
+                dados = dado()
+                print('O número sorteado no dado foi',dados)
                 if 'Mapa para o livro da Salvação' in inventario:
                     hit_marker+=livro_da_salvacao    ##hitmarker fica permanente
                     inventario.append('Livro da Salvação')
                     print('Buscando um milagre.... e com o mapa em mãos Você encontrou o Livro da Salvação')
                     print('Com todo o conhecimento vindo do livro, você será premiado com +5 pontos de vida a cada rodada')
-                elif dado()>=7:
+                elif dados>=7:
                     print('Você encontrou um colega também desesperado')
                     print('Ele demonstra interesse em te ajudar')
                     resposta=input('Aceita a ajuda do colega?')         ######## condições de dados 
@@ -140,7 +143,7 @@ def main():
                         inventario.append('Arquivo secreto do Python')
                     else:
                         print('Deveria ter aceitado...')
-                elif dado()>=16:
+                elif dados>=16:
                     print('Não é possível...')
                     print('Você encontrou o Fone do Pelicano')
                     print('Agora você está mais forte do que o GOKU!')
@@ -164,8 +167,9 @@ def main():
             elif cenario_atual=='Fab Lab':
                 print('Nessa sala temos esses itens:')
                 print(itens_no_cenario('Fab Lab'))
-                print('O número sorteado no dado foi',dado)
-                if dado()>=7:
+                dados = dado()
+                print('O número sorteado no dado foi',dados)
+                if dados>=7:
                     inventario.append('Estilete')
                     inventario.append('Papelão')
                     print('Você ganhou o estilete')
@@ -186,4 +190,4 @@ def main():
                 
                 
 if __name__ == "__main__":
-    main()            
+    main()                                    
